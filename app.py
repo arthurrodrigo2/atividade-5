f0819e6b4ea7d0d762aac7dc25d6de6f52598164
arthurrodrigo2 <arthurrodrigo2@gmail.com>
2024-10-02 21:13:12 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,13 @@
+'''
+Crie uma lista com 15 nomes de cidades, e exiba na tela em ordem alfabética. Ao terminar, envie para o GitHub.
+'''
+
+# Lista de 15 nomes de cidades
+cidades = ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba", "Salvador", "Fortaleza", "Brasília", "Manaus", "Recife", "Porto Alegre", "Goiânia", "Natal", "Florianópolis", "Vitória", "Belém"]
+
+# Ordenando a lista em ordem alfabética
+cidades.sort()
+
+# Exibindo as cidades
+for cidade in cidades:
+    print(cidade)
